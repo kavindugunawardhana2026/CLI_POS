d0def341cd4a54de24455b885c206201e3115bb5
Kavindu Gunawardhana <kavindugunawardhana2026@gmail.com>
2026-08-22 16:55:10 +0530

    initialize application with main menu options

--- Application.java
+++ Application.java
@@ -1,5 +1,17 @@
 class Application{
     public static void main(String args[]){
-        
+        System.out.println("===================================");
+        System.out.println("Hello Welcome to CLI POS");
+        System.out.println("===================================");
+    
+        System.out.println("===================================");
+        System.out.println("Choose an Option (Enter the number): ");
+        System.out.println("===================================");
+
+        System.out.println("1) Customer Management");
+        System.out.println("2) Product Management");
+        System.out.println("3) Order Management");
+        System.out.println("4) Exit");
+
     }
 }
