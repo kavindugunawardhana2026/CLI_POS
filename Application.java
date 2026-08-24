f1810b3f647e4de235470effe9f1fc5521372961
Kavindu Gunawardhana <kavindugunawardhana2026@gmail.com>
2026-08-24 09:08:19 +0530

    enhance customer management with save, update, delete, and search functionalities

--- Application.java
+++ Application.java
@@ -10,7 +10,7 @@ class Application{
         "6) Exit"
     };
 
-    static String [] customers = new String[10]; // save customer names
+    static String [] customers = new String[100]; // save customer names
     static  int customerPointer = 0; // pointer to keep track of the number of customers saved    
     public static void printWelcomeNote(){
         System.out.println("===================================");
@@ -30,22 +30,29 @@ class Application{
         System.out.println("3) Order Management");
         System.out.println("4) Exit");
     }
-
+// Customer Management Section
     public static void manageCustomer(){
         System.out.println("Manage your Customer Informations");
-
+        System.out.println("===================================");
+        System.out.println(Arrays.toString(customers));
         while(true){
+        
             for (String temp : customerQuestions){
                 System.out.println(temp);
             }
             int num = input.nextInt();
+            input.nextLine(); // consume the newline character
 
             switch(num){
                 case 1:
                     saveCustomer();
                     break;
                 case 2:
+                    updateCustomer();
+                    break;  
                 case 3:
+                    deleteCustomer();
+                    break;
                 case 4:
                 case 5:
                 case 6:
@@ -57,20 +64,68 @@ class Application{
     }
 
     public static void saveCustomer(){
-        System.out.println("Enter Customer Name: ");
-        if (customers[9]== null){
-            customers[customerPointer] = input.next();
+        System.out.print("Enter Customer Name "+(customerPointer+1)+": ");
+        if (customers[99]== null){
+            customers[customerPointer] = input.nextLine();
+            System.out.println("Customer " + customers[customerPointer]+ " Saved Successfully.");
             customerPointer++;
-            System.out.println("Customer Saved Successfully.");
+            
         }else{
             System.out.println("Customer List is Full.");
         }
     }
 
+    public static void updateCustomer(){
+        System.out.print("Please Enter the Customer ID: ");
+        int customerIndex = input.nextInt();
+        customerIndex--; // Adjust for 0-based index
+        input.nextLine(); // consume the newline character
+
+        String customerName = customers[customerIndex];
+        if(customerName != null){
+                System.out.println("Customer found and name is: "+customerName);
+                System.out.print("Please Enter the new name: ");
+
+                String tempName = input.nextLine();
+                customers[customerIndex] = tempName;
+                System.out.println("Customer name updated successfully.");
+        }else{
+            System.out.println("Customer not found.");
+        }
+
+    }
+
+    public static void deleteCustomer(){
+        System.out.print("Please Enter the Customer ID: ");
+        int customerIndex = input.nextInt();
+        customerIndex--; // Adjust for 0-based index
+        input.nextLine();
+        if(customers[customerIndex] != null){
+            customers[customerIndex] = null;
+            System.out.println("Customer deleted successfully.");
+        }else{
+            System.out.println("Customer not found.");
+        }
+    }
+
+    public static void searchCustomer(){
+        System.out.print("Please Enter the Search Text: ");
+        String searchText = input.nextLine();
+        for(String tempName : customers){
+            if(tempName!=null && tempName.contains(searchText)){
+                System.out.println("Customer found: "+tempName);
+            }
+        }
+    }
+
+
+    // Product Management Section
     public static void manageProduct(){
         
     }
 
+
+    // Order Management Section
     public static void manageOrder(){
         
     }
